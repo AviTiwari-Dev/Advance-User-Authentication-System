8d554b7de1b30189afbfa8298d0011eedbd44cef
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-16 12:15:56 +0530

    Created data validation models (pydantic models)

---
+++ src/models/data_validation/user_profile_id.py
@@ -0,0 +1,38 @@
+"""
+
+"""
+
+from typing import Annotated
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class UserProfileId(BaseModel):
+    """
+    
+    """
+    user_profile_id: Annotated[
+        UUID,
+        Field(
+            title="User Profile ID",
+            description="User profile id along with other feilds of user profile",
+            deprecated=False,
+            strict=True,
+        )
+    ]
+
+    model_config = ConfigDict(
+        str_to_lower=True,
+        extra="forbid",
+        cache_strings=True,
+        use_enum_values=False,
+        validate_default=True,
+        validate_assignment=True,
+        revalidate_instances=True,
+        # json_schema_extra={
+        #     "examples"=[
+
+        #     ]
+        # },
+    )
