f3aa1c5a9e2efeb6c43bfccbe3d737433d3bdc0e
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-16 17:23:37 +0530

    Created user management engine and added its environment variables

--- src/config/env_variables.py
+++ src/config/env_variables.py
@@ -10,7 +10,17 @@ class Settings(BaseSettings):
     """
     
     """
+    # FastAPI configurations
     DEBUG: bool = True
+
+    # User management engine configurations
+    USER_MANAGEMENT_ENGINE_DATABASE_URL: str = "postgresql+asyncpg://postgres:password123@localhost:5432/user_management"
+    USER_MANAGEMENT_ENGINE_ECHO: bool = False
+    USER_MANAGEMENT_ENGINE_POOL_SIZE: int = 5
+    USER_MANAGEMENT_ENGINE_MAX_OVERFLOW: int = 10
+    USER_MANAGEMENT_ENGINE_POOL_TIMEOUT: int = 30
+    USER_MANAGEMENT_ENGINE_POOL_RECYCLE: int = 1800
+    USER_MANAGEMENT_ENGINE_FUTURE: bool = True
 
     model_config = ConfigDict(
         env_file=".env",
---
+++ src/engines/user_management.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from ..config.env_variables import environment_variables
+
+user_management_engine = create_async_engine(
+    url=environment_variables.USER_MANAGEMENT_ENGINE_DATABASE_URL,
+    echo=environment_variables.USER_MANAGEMENT_ENGINE_ECHO,
+    pool_size=environment_variables.USER_MANAGEMENT_ENGINE_POOL_SIZE,
+    max_overflow=environment_variables.USER_MANAGEMENT_ENGINE_MAX_OVERFLOW,
+    pool_timeout=environment_variables.USER_MANAGEMENT_ENGINE_POOL_TIMEOUT,
+    pool_recycle=environment_variables.USER_MANAGEMENT_ENGINE_POOL_RECYCLE,
+    future=environment_variables.USER_MANAGEMENT_ENGINE_FUTURE,
+)
