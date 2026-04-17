0fb59449c43c31769ade40383171b482e1f854df
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-17 18:52:17 +0530

    Updated SignupBase inheritance.

--- src/models/data_validation/signup_base.py
+++ src/models/data_validation/signup_base.py
@@ -2,19 +2,13 @@
 
 """
 
-from datetime import date
-from typing import Annotated
+from pydantic import ConfigDict
 
-from pydantic import ConfigDict, Field
-
-from src.enums.gender import GenderEnum
-from src.models.data_validation.email_address_base import EmailAddressBase
-from src.models.data_validation.full_name_base import FullNameBase
-from src.models.data_validation.login_base import LoginBase
+from src.models.data_validation.password_base import PasswordBase
 from src.models.data_validation.user_profile_base import UserProfileBase
 
 
-class SignupBase(UserProfileBase, LoginBase):
+class SignupBase(UserProfileBase, PasswordBase):
     """
     
     """
