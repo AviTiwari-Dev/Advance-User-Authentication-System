22ca468f7f405edb7ad6ffca41defe2901c5a7ff
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-18 14:19:31 +0530

    added email_address field in create_user operation.

--- src/operations/create_user.py
+++ src/operations/create_user.py
@@ -7,6 +7,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.models.data_storage.credential import Credential
+from src.models.data_storage.email_address import EmailAddress
 from src.models.data_storage.user import User
 from src.utilities.password_hash import hash_string
 from src.utilities.username import generate_username
@@ -26,45 +27,43 @@ async def create_user(user, db_session: AsyncSession):
     if user.get("middle_name") and not user.get("last_name"):
         raise ValueError("Last name is required when middle name is provided")
     
-    try:
-        for _ in range(5):
-            # Generate unique username of 32 characters
-            username = generate_username(user["first_name"])
+    for _ in range(5):
+        # Generate unique username of 32 characters
+        username = generate_username(user["first_name"])
 
-            # Add user to db
-            try:
-                user_data = User(
-                    username=username,
-                    first_name=user["first_name"],
-                    middle_name=user.get("middle_name"),
-                    last_name=user.get("last_name"),
-                    date_of_birth=user["date_of_birth"],
-                    gender=user["gender"],
-                )
-                db_session.add(user_data)
-                await db_session.flush()
-                user_id = user_data.user_id
+        # Add user to db
+        try:
+            user_data = User(
+                username=username,
+                first_name=user["first_name"],
+                middle_name=user.get("middle_name"),
+                last_name=user.get("last_name"),
+                date_of_birth=user["date_of_birth"],
+                gender=user["gender"],
+            )
+            db_session.add(user_data)
+            await db_session.flush()
+            user_id = user_data.user_id
 
-                # Create credentials
-                credential_data = Credential(
-                    user_id=user_id,
-                    hashed_password=hash_string(user["password"]),
-                )
-                db_session.add(credential_data)
+            # Create email
+            email_data = EmailAddress(
+                user_id=user_id,
+                email_address=user["email_address"],
+            )
+            db_session.add(email_data)
 
-                # Commit transaction
-                await db_session.commit()
+            # Create credentials
+            credential_data = Credential(
+                user_id=user_id,
+                hashed_password=hash_string(user["password"]),
+            )
+            db_session.add(credential_data)
+            await db_session.flush()
 
-                await db_session.refresh(user_data)
-
-                return user_id
-            
-            except IntegrityError:
-                await db_session.rollback()
-                continue
-            
-        raise ValueError("Failed to generate unique username after retries")
+            return user_id
         
-    except Exception:
-        await db_session.rollback()
-        raise
+        except IntegrityError as e:
+            # await db_session.rollback()
+            raise e
+        
+    raise ValueError("Failed to generate unique username after retries")
