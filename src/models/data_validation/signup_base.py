c13b20469678c09684e2baa836ff3dccadff42ca
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-18 14:21:00 +0530

    Corrected import in SignupBase validation model.

--- src/models/data_validation/signup_base.py
+++ src/models/data_validation/signup_base.py
@@ -5,7 +5,7 @@
 from pydantic import ConfigDict
 
 from src.models.data_validation.password_base import PasswordBase
-from models.data_validation.user_base import UserBase
+from src.models.data_validation.user_base import UserBase
 
 
 class SignupBase(UserBase, PasswordBase):
