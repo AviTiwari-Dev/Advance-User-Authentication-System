6938d883ed29a2dbdf0616bfa686b447cb034dbb
Avi Tiwari <117910490+AviTiwari-Dev@users.noreply.github.com>
2026-04-19 21:28:11 +0530

    Changed login base data validation model.

--- src/models/data_validation/login_base.py
+++ src/models/data_validation/login_base.py
@@ -2,26 +2,11 @@
 
 """
 
-from typing import Annotated
-
-from pydantic import BaseModel, Field
-
+from src.models.data_validation.email_address_base import EmailAddressBase
 from src.models.data_validation.password_base import PasswordBase
 
 
-class LoginBase(PasswordBase):
+class LoginBase(EmailAddressBase, PasswordBase):
     """
-    
+
     """
-    username: Annotated[
-        str,
-        Field(
-            title="Username",
-            description="Mandatory unique username",
-            deprecated=False,
-            min_length=2,
-            max_length=32,
-            pattern=r"^[a-z0-9_]+$",
-            strict=True,
-        )
-    ]
